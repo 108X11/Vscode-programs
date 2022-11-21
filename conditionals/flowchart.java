a051e8de669239502320a5a1880b9a1ba3aa46bb
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-11-21 21:39:29 +0800

    commit

--- conditionals/flowchart.java
+++ conditionals/flowchart.java
@@ -2,18 +2,34 @@
 public class flowchart {
     public static void main(String []args){
         
+        //this program checks and prints the greatest value
+
         int x = 5, y = 2, z = 3;
 
+
+        //check if x is greater than y
         if(x > y){
+
+            //this compare if x is greater than z
             if(x > z){
+                //this prints if the statement is true
                 System.out.println(x);
             }else{
+                //this prints if the statement is false
                 System.out.println(z);
             }
+
+            //if the answer is false
         }else{
+
+            //this compare if y is greater than z
             if(y > z){
+
+                //prints if the statement is true
                 System.out.println(y);
             }else{
+
+                //this prints if the statement is false
                 System.out.println(z);
             }
         }
