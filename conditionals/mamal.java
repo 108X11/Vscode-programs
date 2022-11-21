bcd623f9797c6c0e978f58436a6e37a88840d535
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-11-21 22:43:46 +0800

    commit

---
+++ conditionals/mamal.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class mamal {
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+
+    
+
+        System.out.println("Is_Mamal?:");
+        char yes = sc.next().charAt(0);
+
+        System.out.println("No_of_Legs:");
+        int legs = sc.nextInt();
+
+        System.out.println("Can_Bark?:");
+        int ys = sc.next().charAt(0);
+
+        if(yes=='y' || yes == 'Y'){
+            if(ys== 'y' || ys == 'Y'){
+                System.out.println("Dog");
+            }else{
+                System.out.println("Cat");
+            }
+        }else{
+            if(legs == 2){
+            System.out.println("Bird");
+            }else{
+                if(legs == 0){
+                    System.out.println("Fish");
+                }else{
+                System.out.println("UNKNOWN ANIMAL");
+                }
+            }
+        }
+    }
+}
