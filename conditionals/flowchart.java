f2a1437af5e76ee6b9db09461719785fd771242a
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-11-21 21:00:41 +0800

    commit

--- conditionals/flowchart.java
+++ conditionals/flowchart.java
@@ -1,3 +1,20 @@
 public class flowchart {
-    
+    public static void main(String []args){
+
+        int x = 5, y = 2, z = 3;
+
+        if(x > y){
+            if(x > z){
+                System.out.println(x);
+            }else{
+                System.out.println(z);
+            }
+        }else{
+            if(y > z){
+                System.out.println(y);
+            }else{
+                System.out.println(z);
+            }
+        }
+    }
 }
