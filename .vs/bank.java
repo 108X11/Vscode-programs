f32dd8f112578f972742331392d38b65a7e8fab6
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-11-27 15:22:17 +0800

    commit

---
+++ .vs/bank.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class bank{
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    }
+
+    class bankaccount{
+
+        int balance;
+        int 
+    }
+}
