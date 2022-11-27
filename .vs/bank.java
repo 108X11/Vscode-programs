bcfa9c00b07d457de013968b89ea5f5922a7ef88
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-11-27 17:01:26 +0800

    commit

--- .vs/bank.java
+++ .vs/bank.java
@@ -10,6 +10,44 @@ public static void main(String[] args) {
     class bankaccount{
 
         int balance;
-        int 
+        int previoustransact;
+        String custormersName;
+        String cystomersId;
+
+        bankaccount(String cname, String cId){
+
+            custormersName = cname;
+            customersId
+
+        }
+
+        void deposit(int amount){
+
+            if (amount != 0 ){
+                balance = balance + amount;
+                previoustransact = amount;
+            }
+        }
+         void withdrawal (int amount){
+
+            if( amount != 0){
+                balance = balance - amount;
+                previoustransact = amount;
+            }
+         }
+
+         void getprevioustransact(){
+            
+            if (previoustransact > 0) {
+                System.out.println("Deposited" + previoustransact);
+            }else if (previoustransact< 0) {
+                System.out.println("Withdrawn" + Math.abs(previoustransact));
+            }else{
+                System.out.println("No transaction occured");
+            }
+         }
+         void showMenu(){
+
+         }
     }
 }
