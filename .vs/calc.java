d7208a2032fd2d01b76e91cf4e1882d6b6e1390a
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-12-04 19:41:27 +0800

    commit

--- .vs/calc.java
+++ .vs/calc.java
@@ -18,16 +18,17 @@ public class calc{
         System.out.println("4 Division");
         System.out.println("5 Remainder");
 
+        System.out.print("Enter num1: ");
+            num1 = sc.nextInt();
+
+        System.out.print("Enter num2: ");
+            num2 = sc.nextInt();
+
         System.out.print("Enter operator: ");
         choice = sc.nextInt();
 
         switch(choice){
             case 1: 
-            System.out.print("Enter num1: ");
-            num1 = sc.nextInt();
-
-            System.out.print("Enter num2: ");
-            num2 = sc.nextInt();
 
             result = num1 + num2;
             System.out.println("The addition is: " + result);
@@ -35,33 +36,17 @@ public class calc{
 
             case 2:
 
-            System.out.print("Enter num1: ");
-            num1 = sc.nextInt();
-
-            System.out.print("Enter num2: ");
-            num2 = sc.nextInt();
-
             result = num1 - num2;
             System.out.println("The Subtraction is: " + result);
             break;
 
             case 3:
-            System.out.print("Enter num1: ");
-            num1 = sc.nextInt();
-
-            System.out.print("Enter num2: ");
-            num2 = sc.nextInt();
 
             result = num1 * num2;
             System.out.println("The Multiplication is: " + result);
             break;
 
             case 4:
-            System.out.print("Enter num1: ");
-            num1 = sc.nextInt();
-
-            System.out.print("Enter num2: ");
-            num2 = sc.nextInt();
 
             result = num1 / num2;
             System.out.println("The Division is: " + result);
@@ -69,11 +54,6 @@ public class calc{
 
 
             case 5:
-            System.out.print("Enter num1: ");
-            num1 = sc.nextInt();
-
-            System.out.print("Enter num2: ");
-            num2 = sc.nextInt();
 
             result = num1 % num2;
             System.out.println("The Modulo is: " + result);
