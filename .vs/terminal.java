93f4d3874912adaabe5c9a21b75eec86821f72b6
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-12-04 19:47:37 +0800

    commit

---
+++ .vs/terminal.java
@@ -0,0 +1,11 @@
+public class terminal{
+    public static void main(String[] args) {
+        
+        System.out.println("=========================");
+        System.out.println("         TERMINAL        ");
+        System.out.println("    1.Delete             ");
+        System.out.println("    1.Add                ");
+        System.out.println("    1.Subtract           ");
+        System.out.println("=========================");
+    }
+}
